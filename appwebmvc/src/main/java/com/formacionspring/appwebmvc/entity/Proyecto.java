694af760af45d0740d2bda453e8eceb64c15ae0c
JDanielmv <yanda.mv98@gmail.com>
2022-02-17 09:58:06 +0100

    proyecto html

---
+++ appwebmvc/src/main/java/com/formacionspring/appwebmvc/entity/Proyecto.java
@@ -0,0 +1,121 @@
+package com.formacionspring.appwebmvc.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="proyectos")
+public class Proyecto implements Serializable{
+
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	@Column(name="fecha_inicio")
+	@Temporal(TemporalType.DATE)
+	private Date fechaInicio;
+	@Column(name="fecha_fin")
+	@Temporal(TemporalType.DATE)
+	private Date fechaFin;
+	private boolean activo;
+	
+	
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+
+
+
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+
+
+
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+
+
+
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+
+
+
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+
+
+
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
---
+++ appwebmvc/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Jose','Garcia','jg@gmail.com',2134542)
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Calrlos','Perez','cp@gmail.com',43542)
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Maria','Orue','mo@gmail.com',5123)
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Alec','Kiko','ar@gmail.com',893442)
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Alvaro','Moro','jfe@gmail.com',7382542)
+
+INSERT INTO proyectos (nombre,fecha_inicio,fecha_fin,activo) VALUES ('Calrlos',"2022-02-11","2022-02-11",true)
+INSERT INTO proyectos (nombre,fecha_inicio,fecha_fin,activo) VALUES ('Maria',"2022-02-11","2022-02-11",true)
+INSERT INTO proyectos (nombre,fecha_inicio,fecha_fin,activo) VALUES ('Alec',"2022-02-11","2022-02-11",true)
+INSERT INTO proyectos (nombre,fecha_inicio,fecha_fin,activo) VALUES ('Alvaro',"2022-02-11","2022-02-11",true)
